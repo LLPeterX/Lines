7b0710ce8a63423f54396f25ffa1525fb8a501f0
Peter Lebedev <LPeter@mail.ru>
2021-11-15 18:09:25 +0300

    first layout

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 // константы - не менять!
-const BLOCK_SIZE = 50; // ширина одного блока
+const BLOCK_SIZE = 70; // ширина одного блока
+const BALL_SIZE = 50; // диаметр щарика
 const ROWS = 9; // число блоков (матрица NUM * NUM)
+const COLORS = {
+  RED: 'FF0000',
+  BROWN: "FFC0C0",
+  PINK: "F0F0F0",
+  GREEN: "00FF00",
+  BLUE: "0000FF",
+  YELLOW: "F0F0F0",
+  CYAN: "202080"
+};
+let gameOver = false;
 const game = Array(ROWS).fill(0).map(_ => Array(ROWS).fill(0));
+const elBoad = null;
 
-// создание начальной сетки
-function createGrid(eGrid) {
+// создаем игровое поле - сетку ROWS*ROWS
+function createBoard() {
+  elBoard = document.getElementById('board');
   elBoard.innerHTML = "";
-
-}
-
-
-// start
-document.addEventListener('DOMContentLoaded', () => {
-  const elBoard = document.getElementById('board');
-  // создаем игровое поле - сетку ROWS*ROWS
+  gameOver = false;
   for (let y = 0; y < ROWS; y++) {
     for (let x = 0; x < ROWS; x++) {
       game[y][x] = 0;
@@ -24,5 +30,61 @@ document.addEventListener('DOMContentLoaded', () => {
       elBoard.appendChild(cell);
     }
   }
+}
+
+function placeBall(y, x, style, originElement = null, prevY = null, prevX = null) {
+  //let oconsole.log(elBoard.offsetTop);
+  let yOffset = window.scrollY + elBoard.getBoundingClientRect().top; // + Math.floor(BLOCK_SIZE / 2);
+  let xOffset = window.scrollX + elBoard.getBoundingClientRect().left; // + Math.floor(BLOCK_SIZE / 2);
+  //let yOffset = Math.floor(BLOCK_SIZE / 2);
+  // let xOffset = Math.floor(BLOCK_SIZE / 2);
+
+  //debugger;
+  game[y][x] = 1;
+  if (prevY != null && prevX != null) {
+    game[y][x] = 0;
+  }
+  if (originElement == null) {
+    // создать шарик
+    originElement = document.createElement("div");
+    originElement.classList.add("ball");
+    originElement.classList.add(style);
+    elBoard.appendChild(originElement);
+  }
+  originElement.style.left = `${xOffset}px`;
+  originElement.style.top = `${yOffset}px`;
+
+}
+
+// разместить count случайных шариков
+function placeRandomBalls(count) {
+  let freeCells = [], taken = [];
+  // свободные ячейки помещ в массив freeCells
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < ROWS; x++) {
+      if (!game[y][x]) {
+        freeCells.push({ y, x });
+      }
+    }
+  }
+  // если недостаточно свободного места - игра закончена
+  if (freeCells.length <= count) {
+    gameOver = true;
+    return false;
+  }
+  // распределяем шарики по свободным координатам из freeCells
+  let remains = count; // кол-во свободных
+}
+
+// start
+document.addEventListener('DOMContentLoaded', () => {
+
+  // создаем игровое поле - рисуем сетку ROWS * ROWS
+  createBoard();
+  // размещаем сначала 5 случайных шариков
+  //placeRandomBalls(5);
+  placeBall(0, 0, "cyan");
+
+
 
 });
